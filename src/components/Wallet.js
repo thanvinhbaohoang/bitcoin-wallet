d30aa14efbe59c3878c6bf6ecebda961b9efd460
Harold <harold@pop-os.localdomain>
2022-08-15 02:05:49 -0400

    Added Wallet Functions

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -17,21 +17,26 @@ class Wallet extends Component{
         const Mnemonic = require('bitcore-mnemonic');
         const code = new Mnemonic(Mnemonic.Words.ENGLISH);
         const newMnemonicPhrase = code.toString(); //12 words mnemonic phrases
-        var newXpriv = code.toHDPrivateKey();
+        var xpriv = code.toHDPrivateKey(); //Extended Private Key (Used to create PrivateKeys and PubKeys)
 
-        // Generate Address
+        // Generate Address from XPriv
         var value = Buffer.from(newMnemonicPhrase);
         var hash = bitcore.crypto.Hash.sha256(value);
         var bn = bitcore.crypto.BN.fromBuffer(hash);
         var privateKey = new bitcore.PrivateKey(bn);
-        var address = new bitcore.PrivateKey(bn).toAddress();
+        var publicKey = privateKey.toPublicKey();
+
+        // Multiple Ways To Generate Address
+        // var address = privateKey.toAddress(); [We Can use This but not sure ab security]
+        var address= publicKey.toAddress();
         address = address.toString()
 
         // Update State
         this.setState({
             mnemonicPhrase : newMnemonicPhrase,
-            xpriv: newXpriv.toString(),
+            xpriv: xpriv.toString(),
             privateKey: privateKey.toString(),
+            publicKey: publicKey.toString(),
             walletAddress: address.toString(),
         })
 
@@ -39,6 +44,100 @@ class Wallet extends Component{
 
     }
 
+    checkValidPrivateKey() {
+        // // validate an address
+        // if (PrivateKey.isValid(input)){
+        // }
+  
+        // // get the specific validation error that can occurred
+        // var error = PrivateKey.getValidationError(input, Networks.livenet);
+        // if (error) {
+        //     // handle the error
+        // }
+    }
+
+    checkValidPublicKey(){
+        // if (PublicKey.isValid('02a1633cafcc01ebfb6d78e39f687a1f0995c62fc95f51ead10a02ee0be551b5dc')){
+        //     // valid public key
+        //   }
+    }
+
+    checkValidAddress(input){
+        // // validate an address
+        // if (Address.isValid(input){
+        //     ...
+        // }
+        
+        // // validate that an input field is a valid testnet address
+        // if (Address.isValid(input, Networks.testnet){
+        //     ...
+        // }
+        
+        // // validate that an input field is a valid livenet pubkeyhash
+        // if (Address.isValid(input, Networks.livenet, Address.PayToPublicKeyHash){
+        //     ...
+        // }
+        
+        // // get the specific validation error that can occurred
+        // var error = Address.getValidationError(input, Networks.testnet);
+        //     if (error) {
+        //     // handle the error
+        //     }
+        // }
+    }
+
+    createTransaction(){
+        // var privateKey = new bitcore.PrivateKey('L1uyy5qTuGrVXrmrsvHWHgVzW9kKdrp27wBC7Vs6nZDTF2BRUVwy');
+        // var utxo = {
+        //   "txId" : "115e8f72f39fad874cfab0deed11a80f24f967a84079fb56ddf53ea02e308986",
+        //   "outputIndex" : 0,
+        //   "address" : "17XBj6iFEsf8kzDMGQk5ghZipxX49VXuaV",
+        //   "script" : "76a91447862fe165e6121af80d5dde1ecb478ed170565b88ac",
+        //   "satoshis" : 50000
+        // };
+        
+        // var transaction = new bitcore.Transaction()
+        //   .from(utxo)
+        //   .to('1Gokm82v6DmtwKEB8AiVhm82hyFSsEvBDK', 15000)
+        //   .sign(privateKey);  
+    }
+
+    signBTCMessage(){
+        // var Message = require('bitcore-message');
+
+        // var privateKey = new bitcore.PrivateKey('L23PpjkBQqpAF4vbMHNfTZAb3KFPBSawQ7KinFTzz7dxq6TZX8UA');
+        // var message = new Message('This is an example of a signed message.');
+
+        // var signature = message.sign(privateKey);
+    }
+
+    verifyBTCMessage()
+    {
+        // var Message = require('bitcore-message');
+
+        // var address = '13Js7D3q4KvfSqgKN8LpNq57gcahrVc5JZ';
+        // var signature = 'IBOvIfsAs/da1e36W8kw1cQOPqPVXCW5zJgNQ5kI8m57FycZXdeFmeyoIqJSREzE4W7vfDmdmPk0HokuJPvgPPE=';
+        
+        // var verified = new Message('This is an example of a signed message.').verify(address, signature);
+    }
+
+    createOPReturnTransaction()
+    {
+        // var privateKey = new bitcore.PrivateKey('L1uyy5qTuGrVXrmrsvHWHgVzW9kKdrp27wBC7Vs6nZDTF2BRUVwy');
+        // var utxo = {
+        //   "txId" : "115e8f72f39fad874cfab0deed11a80f24f967a84079fb56ddf53ea02e308986",
+        //   "outputIndex" : 0,
+        //   "address" : "17XBj6iFEsf8kzDMGQk5ghZipxX49VXuaV",
+        //   "script" : "76a91447862fe165e6121af80d5dde1ecb478ed170565b88ac",
+        //   "satoshis" : 50000
+        // };
+        
+        // var transaction = new bitcore.Transaction()
+        //     .from(utxo)
+        //     .addData('bitcore rocks') // Add OP_RETURN data
+        //     .sign(privateKey);    
+    }
+    
     renderMnemonicPhrase(){
         return (
            <div>
@@ -54,7 +153,9 @@ class Wallet extends Component{
                 <p>
                     PrivateKey: <br/>{this.state.privateKey}
                 </p>
-
+                <p>
+                    PublicKey: <br/>{this.state.publicKey}
+                </p>
                 <p>
                     Address: <br/> {this.state.walletAddress}
                 </p>
@@ -63,23 +164,6 @@ class Wallet extends Component{
     }
 
  
-
-    createTransaction() {
-        var privateKey = new bitcore.PrivateKey('L1uyy5qTuGrVXrmrsvHWHgVzW9kKdrp27wBC7Vs6nZDTF2BRUVwy');
-        var utxo = {
-        "txId" : "115e8f72f39fad874cfab0deed11a80f24f967a84079fb56ddf53ea02e308986",
-        "outputIndex" : 0,
-        "address" : "17XBj6iFEsf8kzDMGQk5ghZipxX49VXuaV",
-        "script" : "76a91447862fe165e6121af80d5dde1ecb478ed170565b88ac",
-        "satoshis" : 50000
-        };
-
-        var transaction = new bitcore.Transaction()
-        .from(utxo)
-        .to('1Gokm82v6DmtwKEB8AiVhm82hyFSsEvBDK', 15000)
-        .sign(privateKey);
-    }
-
 
     
     render() {
